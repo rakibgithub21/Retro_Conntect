a09e9c98fb3e27d4862d6d907e5a8d9c800017a9
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-03 10:01:43 +0600

    display alldata dynamically

---
+++ index.js
@@ -0,0 +1,77 @@
+const leftSideDynamicContainer = document.getElementById('dynamic-container');
+
+const loadAllPosts = async () => {
+    const url = `https://openapi.programming-hero.com/api/retro-forum/posts`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayAllPosts(data.posts);
+}
+
+const displayAllPosts = (posts) => {
+    posts.forEach(post => {
+        // console.log(post);
+        const mainDiv = document.createElement('div');
+        mainDiv.classList = `flex gap-6 bg-[#797DFC] bg-opacity-20 p-10 rounded-2xl mb-5`
+        mainDiv.innerHTML = `
+        <div class="relative">
+                            <img class="w-20 md:w-14 rounded-full" src=${post.image}>
+                            <div class="w-3 h-3 rounded-full bg-green-600 absolute top-0 ">
+
+                            </div>
+                        </div>
+        <div class="space-y-2">
+
+                            <div class="flex gap-5 font-medium text-sm inter">
+                                <p># <span>${post.category}</span></p>
+                                <p>Author: <span>${post.author.name}</span></p>
+                            </div>
+
+                            <h3 class="mulish md:text-xl text-[#12132D] font-bold">${post.title}</h3>
+                            <p class="text-[#12132d99] inter ">${post.description}</p>
+                            <hr class="border-dashed border-[#12132d99]">
+                            <div class="flex justify-between text-[#12132d99] inter">
+                                <div class="flex gap-4">
+                                    <p><i class="fa-regular fa-message"></i> <span>${post.comment_count}</span></p>
+                                    <p><i class="fa-regular fa-eye"></i> <span>${post.view_count}</span></p>
+                                    <p></p>
+                                    <p><i class="fa-regular fa-clock"></i> <span>${post.posted_time}</span> min</p>
+                                </div>
+                                <div>
+                                    <button class="btn bg-[#10B981] btn-circle">
+                                        <i class="fa-regular fa-envelope-open text-white font-bold text-lg"></i>
+                                    </button>
+                                </div>
+                            </div>
+
+                        </div>
+        
+        `;
+        leftSideDynamicContainer.appendChild(mainDiv)
+
+    });
+}
+
+
+
+const loadLatestPosts = async () => {
+    const url = `https://openapi.programming-hero.com/api/retro-forum/latest-posts`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayLatestPosts(data);
+}
+
+
+const displayLatestPosts = (data) => {
+    console.log(data);
+}
+
+
+
+
+
+
+
+
+loadAllPosts();
+
+loadLatestPosts()
